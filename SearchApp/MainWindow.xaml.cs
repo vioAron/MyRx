7099790d57b7a631fb29553735276ec16506abd3
Viorel Aron <vio.aron@yahoo.com>
2014-12-31 13:44:42 +0200

    search with rx
    - without takeUntil

--- SearchApp/MainWindow.xaml.cs
+++ SearchApp/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Threading;
 using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace SearchApp
 {
@@ -10,14 +15,52 @@ namespace SearchApp
     /// </summary>
     public partial class MainWindow
     {
+        private readonly IScheduler _uiScheduler;
+
+        private static readonly List<string> _strings = new List<string> { "aaaaaa", "bbbbbbbbb", "cccccccc", "aaaaabbbb", "aaaaacccccc" };
+
+        private static IObservable<string> GetSearchResult(string searchText)
+        {
+            return Observable.Create<string>(observer =>
+            {
+                _strings.ForEach(s =>
+                {
+                    if (s.Contains(searchText))
+                    {
+                        Thread.Sleep(1000);
+                        observer.OnNext(s);
+                    }
+                });
+
+                observer.OnCompleted();
+
+                return Disposable.Empty;
+            });
+        }
+
         public MainWindow()
         {
             InitializeComponent();
 
-            IObservable<EventPattern<TextChangedEventArgs>> textChangedObservable = Observable.FromEventPattern<TextChangedEventHandler, TextChangedEventArgs>(
+            _uiScheduler = new SynchronizationContextScheduler(SynchronizationContext.Current);
+
+            var textChangedObservable = Observable.FromEventPattern<TextChangedEventHandler, TextChangedEventArgs>(
                 ev => SearchTextBox.TextChanged += ev, ev => SearchTextBox.TextChanged -= ev);
 
-            textChangedObservable.Subscribe(args => SubscribeTextBlock.Text += Environment.NewLine + SearchTextBox.Text);
+            textChangedObservable.Select(ep => ((TextBox)ep.Sender).Text).Throttle(TimeSpan.FromSeconds(5)).DistinctUntilChanged().Subscribe(
+                args =>
+                {
+                    var resultObservable = GetSearchResult(args);
+                    resultObservable.Buffer(2).ObserveOn(_uiScheduler).Subscribe(results =>
+                    {
+                        foreach (var result in results)
+                        {
+                            SubscribeTextBlock.Text += Environment.NewLine +
+                                                   string.Format("{0} - {1}", DateTime.Now.ToLongTimeString(),
+                                                       result);
+                        }
+                    });
+                });
         }
     }
 }
