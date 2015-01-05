1a3de0b6089b5451c78532a985223b24a9e478a4
Viorel Aron <viorel.aron@ullink.com>
2015-01-05 11:11:43 +0200

    add from event pattern app

--- FromEventPatternApp/Form1.Designer.cs
+++ FromEventPatternApp/Form1.Designer.cs
@@ -28,12 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f"});
+            this.comboBox1.Location = new System.Drawing.Point(41, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- FromEventPatternApp/Form1.cs
+++ FromEventPatternApp/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FromEventPatternApp
@@ -17,13 +10,7 @@ namespace FromEventPatternApp
         {
             InitializeComponent();
 
-            MouseClick +=Form1_MouseClick;
-            Observable.FromEventPattern<MouseEventArgs>(ev => MouseClick += ev, ev => MouseClick -= ev)
-        }
-
-        void Form1_MouseClick(object sender, MouseEventArgs e)
-        {
-            throw new NotImplementedException();
+            Observable.FromEventPattern<MouseEventHandler, MouseEventArgs>(ev => comboBox1.MouseWheel += ev, ev => comboBox1.MouseWheel -= ev).Subscribe(args => Console.WriteLine(args.EventArgs.Delta));
         }
     }
 }
