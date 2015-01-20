cd5109ca314bb1b62f6e92c2f84e19f939a99508
Viorel Aron <viorel.aron@ullink.com>
2015-01-20 09:34:10 +0200

    Add more time shifted methods

--- TimeShiftedSeqApp/Program.cs
+++ TimeShiftedSeqApp/Program.cs
@@ -7,9 +7,46 @@ namespace TimeShiftedSeqApp
     {
         static void Main()
         {
-            Buffer();
+            //Buffer();
+
+            //Delay();
+
+            //Sample();
+
+            Timeout();
 
             Console.ReadKey();
+        }
+
+        private static void Timeout()
+        {
+            var source = Observable.Interval(TimeSpan.FromMilliseconds(100)).Take(10)
+                                   .Concat(Observable.Interval(TimeSpan.FromSeconds(2)));
+            
+            var timeout = source.Timeout(TimeSpan.FromSeconds(1));
+
+            timeout.Subscribe(Console.WriteLine, Console.WriteLine, () => Console.WriteLine("Completed"));
+        }
+
+        private static void Sample()
+        {
+            var source = Observable.Interval(TimeSpan.FromMilliseconds(150));
+
+            var onePerSecond = source.Sample(TimeSpan.FromSeconds(1));
+
+            Console.WriteLine("~ started at {0}", DateTime.Now.TimeOfDay);
+
+            onePerSecond.Subscribe(l => Console.WriteLine("value = {0} at {1}", l, DateTime.Now.TimeOfDay));
+        }
+
+        private static void Delay()
+        {
+            var source = Observable.Interval(TimeSpan.FromSeconds(1));
+            var delayed = source.Delay(TimeSpan.FromSeconds(10));
+
+            Console.WriteLine("~ started at {0}", DateTime.Now.TimeOfDay);
+
+            delayed.Subscribe(l => Console.WriteLine(DateTime.Now.TimeOfDay));
         }
 
         private static void Buffer()
