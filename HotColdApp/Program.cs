18b658a097af2e4def533c0136052844dc5caed8
Viorel Aron <viorel.aron@ullink.com>
2015-01-22 10:27:05 +0200

    Add RefCount PublishLast Replay Multicast

--- HotColdApp/Program.cs
+++ HotColdApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading;
 
 namespace HotColdApp
@@ -15,14 +16,78 @@ namespace HotColdApp
 
             //ShareACold();
 
-            RefCount();
+            //RefCount();
+
+            //PublishLast();
+
+            //Replay();
+
+            Multicast();
 
             Console.ReadKey();
         }
 
+        private static void Multicast()
+        {
+            var period = TimeSpan.FromSeconds(1);
+
+            //var observable = Observable.Interval(period).Publish();
+
+            var observable = Observable.Interval(period);
+            var shared = new Subject<long>();
+            shared.Subscribe(i => Console.WriteLine("first subscription : {0}", i));
+            observable.Subscribe(shared);   //'Connect' the observable.
+            Thread.Sleep(period);
+            Thread.Sleep(period);
+            Thread.Sleep(period);
+            shared.Subscribe(i => Console.WriteLine("second subscription : {0}", i));
+        }
+
+        private static void Replay()
+        {
+            var hot = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5).Do(l => Console.WriteLine("publishing {0}", l)).Publish();
+
+            hot.Connect();
+
+            var replay = hot.Replay();
+
+            replay.Connect();
+
+            var first = replay.Subscribe(i => Console.WriteLine("first subscription : {0}", i));
+
+            var second = replay.Subscribe(i => Console.WriteLine("second subscription : {0}", i));
+
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+
+            var third = replay.Subscribe(i => Console.WriteLine("third subscription : {0}", i));
+        }
+
+        private static void PublishLast()
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5).Do(l => Console.WriteLine("publishing {0}", l)).PublishLast();
+
+            observable.Connect();
+
+            var first = observable.Subscribe(i => Console.WriteLine("first subscription : {0}", i));
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            Console.WriteLine("dispose first!");
+
+            first.Dispose();
+        }
+
         private static void RefCount()
         {
-            throw new NotImplementedException();
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Do(l => Console.WriteLine("publishing {0}", l)).Publish().RefCount();
+
+            var first = observable.Subscribe(i => Console.WriteLine("first subscription : {0}", i));
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            Console.WriteLine("dispose first!");
+
+            first.Dispose();
         }
 
         private static void ShareACold()
