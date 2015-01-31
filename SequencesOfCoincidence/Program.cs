ae40234ca226d6a1da5920ccecf8b4caaac749de
Viorel Aron <vio.aron@yahoo.com>
2015-01-31 20:25:18 +0200

    Add sequences of coincidence app

---
+++ SequencesOfCoincidence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace SequencesOfCoincidence
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Window();
+
+            Console.ReadKey();
+        }
+
+        private static void Window()
+        {
+            var ten = Observable.Interval(TimeSpan.FromSeconds(1)).Take(10);
+
+            var windowCount = -1;
+            ten.Window(3).Subscribe(window =>
+            {
+                windowCount++;
+                window.Subscribe(value => Console.WriteLine("window{0}, value{1}", windowCount, value));
+            });
+        }
+    }
+}
